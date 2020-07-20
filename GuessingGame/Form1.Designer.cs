1fd0912ce8eb7253e6e4e3772f51d145d8f4e594
Local User <Local User@DESKTOP-N1NMMQ0>
2020-07-20 01:41:24 -0400

    Add project files.

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿//Programmer: Austin Parker
+//Date: May 20,2020
+//Purpose: Creates a guessing game where users can input any number
+//of guesses and colors change based on user input.
+
+
+namespace GuessingGame
+{
+    partial class GuessingGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+
+
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGuessCount = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGuessNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            
+            // label1
+            
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Guess Count:";
+            
+            // lblGuessCount
+            
+            this.lblGuessCount.AutoSize = true;
+            this.lblGuessCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessCount.Location = new System.Drawing.Point(116, 56);
+            this.lblGuessCount.Name = "lblGuessCount";
+            this.lblGuessCount.Size = new System.Drawing.Size(14, 13);
+            this.lblGuessCount.TabIndex = 1;
+            this.lblGuessCount.Text = "0";
+            
+            // btnGuess
+            
+            this.btnGuess.Location = new System.Drawing.Point(15, 88);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            
+            // btnReset
+            
+            this.btnReset.Location = new System.Drawing.Point(119, 88);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            
+            // label2
+            
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Number:";
+            
+            // txtGuessNumber
+            
+            this.txtGuessNumber.Location = new System.Drawing.Point(119, 26);
+            this.txtGuessNumber.Name = "txtGuessNumber";
+            this.txtGuessNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtGuessNumber.TabIndex = 5;
+            
+            // Form1
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 145);
+            this.Controls.Add(this.txtGuessNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblGuessCount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Guess Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGuessCount;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGuessNumber;
+    }
+}
+
+
+
+
+
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,88 @@
+﻿//Programmer: Austin Parker
+//Date: May 20,2020
+//Purpose: Creates a guessing game where users can input any number
+//of guesses and colors change based on user input.
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class GuessingGame : Form
+    {
+
+        //Global variables for guess count and guess number
+        private static int guessCount = 0;
+        private static int guessNumber = 0;
+        public GuessingGame()
+        {
+            InitializeComponent();
+
+            //This will show the hint when opening the application
+            if (guessCount <= 0)
+            {
+                MessageBox.Show("Hint: The guess number is between 0 and 101");
+
+                Random randNum = new Random();
+                //Random number between 1 to 100
+                guessNumber = randNum.Next(0, 101);
+            }
+        }
+
+        //Guess button click event
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //calls guess number method
+            GuessNumber();
+        }
+
+        //Reset button click event
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Resets all the inputs of the application
+            txtGuessNumber.Text = string.Empty;
+            lblGuessCount.Text = "0";
+            this.BackColor = Color.Empty;
+            guessCount = 0;
+            guessNumber = 0;
+        }
+
+        //The method for guessing the number
+        private void GuessNumber()
+        {
+            int numberGuess;
+
+            //This will only proocess if user enters a valid number
+            if (int.TryParse(txtGuessNumber.Text, out numberGuess))
+            {
+                guessCount++;
+
+                //If number is higher than the random number, shows light blue background
+                if (numberGuess > guessNumber)
+                {
+                    this.BackColor = Color.LightBlue;
+                }
+                //If number is lower than the random number, shows light red background
+                else if (numberGuess < guessNumber)
+                {
+                    this.BackColor = Color.PaleVioletRed;
+                }
+                //Displays green when user enters correct number
+                else if (numberGuess == guessNumber)
+                {
+                    this.BackColor = Color.LightGreen;
+                    MessageBox.Show("You guessed the correct number! " + guessNumber);
+                }
+
+                //Display total number of guesses
+                lblGuessCount.Text = guessCount.ToString();
+            }
+            //Alert for non valid number 
+            else
+            {
+                MessageBox.Show("Enter a valid number!");
+            }
+        }
+    }
+}
